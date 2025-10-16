67741fe119eeefbdef169c969b144e3bb3a557b1
wendyxiang36512 <165988229+wendyxiang36512@users.noreply.github.com>
2025-10-15 21:40:53 -0700

    Update reading_the_chain.py

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -102,22 +102,21 @@ def is_ordered_block(w3, block_num):
 
 
 def get_contract_values(contract, admin_address, owner_address):
-	"""
-	Takes a contract object, and two addresses (as strings) to be used for calling
-	the contract to check current on chain values.
-	The provided "default_admin_role" is the correctly formatted solidity default
-	admin value to use when checking with the contract
-	To complete this method you need to make three calls to the contract to get:
-	  onchain_root: Get and return the merkleRoot from the provided contract
-	  has_role: Verify that the address "admin_address" has the role "default_admin_role" return True/False
-	  prime: Call the contract to get and return the prime owned by "owner_address"
+    """
+    Takes a contract object, and two addresses (as strings) to be used for calling
+    the contract to check current on chain values.
+    The provided "default_admin_role" is the correctly formatted solidity default
+    admin value to use when checking with the contract
+    To complete this method you need to make three calls to the contract to get:
+      onchain_root: Get and return the merkleRoot from the provided contract
+      has_role: Verify that the address "admin_address" has the role "default_admin_role" return True/False
+      prime: Call the contract to get and return the prime owned by "owner_address"
 
-	check on available contract functions and transactions on the block explorer at
-	https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
-	"""
-	  default_admin_role = int.to_bytes(0, 32, byteorder="big")
+    check on available contract functions and transactions on the block explorer at
+    https://testnet.bscscan.com/address/0xaA7CAaDA823300D18D3c43f65569a47e78220073
+    """
+    default_admin_role = int.to_bytes(0, 32, byteorder="big")
 
-	  # complete the following lines by performing contract calls
     # Get and return the merkleRoot from the provided contract (bytes32)
     onchain_root = contract.functions.merkleRoot().call()
 
